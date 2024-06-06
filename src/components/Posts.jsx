253d6719c50296b3b40adbc713b7a3c77ec62b12
MINT <mint.2978@gmail.com>
2024-06-06 15:33:10 +0700

    feat: add post blog

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,31 +1,59 @@
-function Posts() {
+import data from "./data.jsx";
+import { useState } from "react";
+const PostListItem = (props) => {
+  const [like, setLike] = useState(props.item.likes);
+  const handleLike = () => {
+    if (like >= 0) {
+      setLike(like + 1);
+    } else {
+      setLike(0);
+    }
+  };
+  const handleDislike = () => {
+    if (like >= 0) {
+      setLike(like - 1);
+    } else {
+      setLike(0);
+    }
+  };
   return (
-    <div class="app-wrapper">
-      <h1 class="app-title">Posts</h1>
-      <div class="post-list">
-        <div class="post-item">
-          <div class="post-header">
-            <h2>Post Title #1</h2>
-            <div class="post-social-media-stats">
-              <span class="stats-topic">Likes: </span>
-              <span class="post-likes">10</span>
-            </div>
-          </div>
-          <p class="post-content">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
-            vel turpis vestibulum, aliquet ipsum vitae, auctor risus. Morbi
-            tincidunt, leo non molestie consectetur, elit libero faucibus
-            tellus, sed fringilla tortor libero sit amet odio. Maecenas sed ante
-            condimentum mauris euismod pellentesque eu eu justo...
-          </p>
-          <div class="post-actions">
-            <button class="like-button">Like</button>
-            <button class="dislike-button">Dislike</button>
-          </div>
+    <div class="post-item">
+      <div class="post-header">
+        <h2>Post Title #{props.item.id}</h2>
+        <div class="post-social-media-stats">
+          <span class="stats-topic">Likes: {like}</span>
         </div>
+      </div>
+      <p class="post-content">{props.item.content}</p>
+      <div class="post-actions">
+        <button class="like-button" onClick={handleLike}>
+          Like
+        </button>
+        <button class="dislike-button" onClick={handleDislike}>
+          Dislike
+        </button>
       </div>
     </div>
   );
-}
+};
+
+const PostList = () => {
+  return (
+    <div class="post-list">
+      {data.map((item, index) => (
+        <PostListItem key={index} item={item} />
+      ))}
+    </div>
+  );
+};
+
+const Posts = () => {
+  return (
+    <div class="app-wrapper">
+      <h1 class="app-title">Posts</h1>
+      <PostList />
+    </div>
+  );
+};
 
 export default Posts;
